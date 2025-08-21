6c5c0daddcd857441f8524da336db7999c809a54
GustavoBenito <160787099+GustavoBenito12@users.noreply.github.com>
2025-08-21 23:37:44 +0000

    aula

--- testes_an/src/app/app-routing-module.ts
+++ testes_an/src/app/app-routing-module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LivrosModule } from './livros/livros-module';
+import { ContadorModule } from './contador/contador-module';
 
 const routes: Routes = [{ path: 'livros', loadChildren: () => import('./livros/livros-module').then(m => m.LivrosModule) }
 ];
